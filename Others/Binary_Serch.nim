16153bd719f2ef1c02da879ed5d5c35673f3dae5
sukenori <sukenori@coo.net>
2025-04-10 13:23:44 +0000

    二分探索を微修正

--- Others/Binary_Serch.nim
+++ Others/Binary_Serch.nim
@@ -4,12 +4,12 @@ minLeft((x:int)=>f(x):bool,a..b)
 
 var
   l=min
-  r=max
+  r=max+1
 while r-l>1:
   let m=(l+r) div 2
-  if m<k: l=m
+  if m<=k: l=m
   else: r=m
-echo r
-#絶対満たすのはl側かr側か（最大値を求めるのか、最小値を求めるのか）
+echo l
+#絶対満たすのはl側かr側か
 #範囲を決める際には満たす側がとりうる範囲をベースに、満たさない側が1外に出るようにする
 #中央が満たすときには満たす側を動かし、最終的な答えも満たす側になる
