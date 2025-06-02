8405edada23993cdac6053ebafe3abd1d6aa36ca
sukenori <sukenori@coo.net>
2025-06-02 14:07:07 +0000

    組合せの文字修正

--- Math/Combination.nim
+++ Math/Combination.nim
@@ -1,5 +1,6 @@
 var t:Table[(int,int),mint]
-proc c(i,j:int):mint=
-  if not t.hasKey((i,j)):
-    t[(i,j)]=(if i==j: 1.mint else: c(i,j+1)/(i-j)*(j+1))
-  t[(i,j)]
+proc c(n,r:int):mint=
+  #if r<0 or r>n: return 0.mint
+  if not t.hasKey((n,r)):
+    t[(n,r)]=if r==0 or r==n: 1.mint else: c(n-1,r-1)+c(n-1,r)
+  return t[(n,r)]
