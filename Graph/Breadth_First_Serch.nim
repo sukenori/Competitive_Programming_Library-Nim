f5b9d86cfc0c146cb2287d27af0e0fd8ccf7a0fa
sukenori <sukenori@coo.net>
2025-07-27 12:51:27 +0000

    BFSとN進数を追補

--- Graph/Breadth_First_Serch.nim
+++ Graph/Breadth_First_Serch.nim
@@ -2,9 +2,10 @@ import deques
 var
   q=[0].toDeque
   d=false.repeat(N)
+d[0]=true
 while q.len>0:
   let i=q.popFirst
-  d[i]=true
   for j in g[i]:
     if not d[j.t]:
+      d[j.t]=true
       q.addLast(j.t)
--- Math/N-adic_Number.nim
+++ Math/N-adic_Number.nim
@@ -1,5 +1,23 @@
 var a:seq[int]
 while n>0: a.insert(n mod b,0); n=n div b
 
+import deques
+var a:Deques[int]
+while n>0: a.addFirst(n mod b); n=n div b
+a.toSeq
+
 var a=0.repeat(d)
 var i=0; while n>0: a[i]=n mod b; n=n div b; i+=1
+
+for n in (0..<N).toSeq.repeat(K).product.mapIt(it.join).sorted:
+  echo n
+
+var
+  n=0.repeat(K)
+  i=1
+while i<=K:
+  if i==1: echo n
+  n[^i]+=1
+  if n[^i]==N:
+    n[^i]=0; i+=1 
+  else: i=1
